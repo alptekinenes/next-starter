e1c673eaf8dcdc9667b3a19b4ebd98e703493506
dnomak <me@dnomak.com>
2023-01-15 01:22:39 +0300

    Add app index.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/assets/styles/index.scss'
 import type { AppProps } from 'next/app'
 
-import '@/app/modules/font-awesome'
+import '@/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
