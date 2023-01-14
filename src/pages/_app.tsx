2cd3f3d9089a9f122cd5af353bcc5f3c2f01b626
dnomak <me@dnomak.com>
2023-01-14 03:00:32 +0300

    Add Font Awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/assets/styles/index.scss'
 import type { AppProps } from 'next/app'
 
+import '@/app/modules/font-awesome'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { findIconDefinition } from '@fortawesome/fontawesome-svg-core'
+const fabReact = findIconDefinition({ prefix: 'fab', iconName: 'react' })
+
 export default function Home() {
   return (
     <>
@@ -9,7 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>next-starter</main>
+      <main>
+        <FontAwesomeIcon icon={fabReact} />
+        next-starter
+      </main>
     </>
   )
 }
